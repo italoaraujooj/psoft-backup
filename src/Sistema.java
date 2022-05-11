ff071bf8200533d3138b68f28c8f10a03a48d170
Ítalo Ramalho <matheus.ramalho1354@gmail.com>
2022-05-11 11:52:55 -0300

    Adição de Repositórios

---
+++ src/Sistema.java
@@ -0,0 +1,24 @@
+import exemplo.Lote;
+import exemplo.Produto;
+import exemplo.ProdutoRepository;
+
+public class Sistema {
+
+	public static void main(String[] args) {
+		
+		Produto leite = new Produto("Leite", "Parmalat");
+		Produto pao = new Produto("Pão", "Poli Massas");
+		Produto ovo = new Produto("Ovo", "Granja Comary");
+		Lote loteLeite = new Lote(leite, 10L);
+		Lote lotePao = new Lote(pao, 30L);
+		Lote loteOvo = new Lote(ovo, 20L);
+		ProdutoRepository catalogo = new ProdutoRepository();
+		String idLeite = catalogo.addProduto(leite);
+		String idPao = catalogo.addProduto(pao);
+		String idOvo = catalogo.addProduto(ovo);
+		Produto novoLeite = catalogo.getProduto(idLeite);
+		novoLeite.setFabricante("Italac");
+		catalogo.atualizaProduto(novoLeite);
+		System.out.println(catalogo.listaProdutos());
+	}
+}
